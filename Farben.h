74d80e1048a255065d76735168a8e2381b9cce58
Hollenda <130311536+Hollenda@users.noreply.github.com>
2023-04-09 21:05:35 +0200

    Add files via upload

---
+++ Farben.h
@@ -0,0 +1,18 @@
+
+void initialize_color(void);
+
+void reset_color(void);
+
+void red(void);
+
+void green(void);
+
+void yellow(void);
+
+void blue(void);
+
+void purple(void);
+
+void cyan(void);
+
+void white(void);
